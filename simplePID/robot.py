1b98e0e6348f0de1e244abcec30d653d91f7e39b
Mr.R^2 <jrothritchie@hdsd.org>
2020-02-09 15:43:06 -0500

    Updated PID example

--- simplePID/robot.py
+++ simplePID/robot.py
@@ -1,15 +1,16 @@
 import wpilib
 from wpilib import drive
+from networktables import NetworkTables
 from wpilib.controller import PIDController
 import ctre
 
-from navx import AHRS
+import navx
 
 
 class MyRobot(wpilib.TimedRobot):
     if wpilib.RobotBase.isSimulation():
             # These PID parameters are used in simulation
-            kP = 0.06
+            kP = .06
             kI = 0.00
             kD = 0.00
             kF = 0.00
@@ -23,11 +24,12 @@ class MyRobot(wpilib.TimedRobot):
     kToleranceDegrees = 2.0
 
     def robotInit(self):
+        self.sd = NetworkTables.getTable("SmartDashboard")
         self.left_motor = ctre.WPI_TalonSRX(0)
         self.right_motor = ctre.WPI_TalonSRX(1)
         self.drive = wpilib.drive.DifferentialDrive(self.left_motor, self.right_motor)
         self.stick = wpilib.Joystick(0)
-        self.ahrs = AHRS.create_spi()
+        self.navx = navx.AHRS.create_spi()
         # self.ahrs = AHRS.create_i2c()
 
         turnController = PIDController(
@@ -36,9 +38,6 @@ class MyRobot(wpilib.TimedRobot):
 
         self.turnController = turnController
         self.rotateToAngleRate = 5
-        self.drive.setExpiration(0.1)
-        self.tm = wpilib.Timer()
-        self.tm.start()
     def autonomousInit(self):
          """Executed at the start of teleop mode"""
          self.drive.setSafetyEnabled(True)
@@ -46,17 +45,14 @@ class MyRobot(wpilib.TimedRobot):
     def autonomousPeriodic(self):
 
 
-        #self.MyRobot.setSafetyEnabled(True)
-        time = self.tm.get()
+        
+        self.turnController.setSetpoint(0)
+        pidOutput = self.turnController.calculate(self.navx.getAngle())
 
-        if (time >= (1)) and (time<=(25)):
-                print("NavX Gyro", self.ahrs.getYaw(), self.ahrs.getAngle())
-                self.drive.tankDrive(0, self.turnController.calculate(self.ahrs.getYaw()), True)
-                print (self.turnController.calculate(self.ahrs.getYaw()))
-        else:
-            self.drive.tankDrive(
-                0, 0, True
-            )
+        
+
+        self.drive.arcadeDrive(pidOutput,0)
+        self.sd.putNumber("Field Angle",self.navx.getAngle())
         
 
 if __name__ == "__main__":
